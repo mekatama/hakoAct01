e26ed40c47e396e2288eaffeacbac1de52c9ecae
mekatama <tamatte@hotmail.com>
2018-04-03 19:27:56 +0900

    重力の処理

---
+++ Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	CharacterController characterController;//コンポーネントを入れる用
+	Animator animator;
+	Vector3 moveDirection = Vector3.zero;
+	public float graviy;
+	public float speedZ;
+
+	void Start () {
+		characterController = GetComponent<CharacterController>();	//コンポーネントを取得
+//		animator = GetComponent<Animator>();						//コンポーネントを取得
+	}
+	
+	void Update () {
+		//地上にいる時の処理
+		if(characterController.isGrounded){
+//			moveDirection.y -= graviy * Time.deltaTime;	//重力の加算
+		}
+		//地上にいる時
+		else{
+			moveDirection.y -= graviy * Time.deltaTime;	//重力の加算
+		}
+
+		//移動実行
+		Vector3 globalDirection = transform.TransformDirection(moveDirection);
+		characterController.Move(globalDirection * Time.deltaTime);
+	}
+}
