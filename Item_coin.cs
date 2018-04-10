20fbe7564e8028c356802ff7e9ce38e0c2393640
mekatama <tamatte@hotmail.com>
2018-04-10 19:37:03 +0900

    コインの数を加算処理

--- Item_coin.cs
+++ Item_coin.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Item_coin : MonoBehaviour {
-//	public int item_score1;		//アイテムの点数
-//	GameObject gameController;	//スコア表示を管理しているオブジェクト入れる用
+	public int coin;			//coinの数
+	GameObject gameController;	//gameController入れる用
 
 	void Start () {
-//		gameController = GameObject.FindWithTag ("GameController");		//GameControllerオブジェクトを探す
+		coin = 1;	//初期化
+		gameController = GameObject.FindWithTag ("GameController");		//GameControllerオブジェクトを探す
 	}
 
 	//他のオブジェクトとの当たり判定
 	void OnTriggerEnter(Collider other) {
 		if(other.tag == "Player"){
-			//スコア加算
+			//coin加算
 			//gcって仮の変数にGameControllerのコンポーネントを入れる
-//			GameController gc = gameController.GetComponent<GameController>();
+			GameController gc = gameController.GetComponent<GameController>();
 			//GameControllerで管理しているスコア表示用の変数を使って計算
-//			gc.total_Score = gc.total_Score + item_score1;
+			gc.num_coin = gc.num_coin + coin;
 			//このGameObjectを［Hierrchy］ビューから削除する
-			Debug.Log("item get");
 			Destroy(gameObject);
 		}
 	}
