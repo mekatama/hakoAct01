6929d3b8acee5336fb668d6286564300eb82ce89
mekatama <tamatte@hotmail.com>
2018-04-10 19:37:58 +0900

    ゲーム内で使う数値等の制御スクリプト

---
+++ GameController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public int num_coin = 0;	//coinの数
+
+	void Start () {
+	}
+	
+	void Update () {
+		Debug.Log("coin;" + num_coin);
+	}
+}
