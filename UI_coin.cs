ab98e78a83f81f679f23e9b1656cb4d6825fdee8
mekatama <tamatte@hotmail.com>
2018-04-12 19:23:43 +0900

    coin数表示処理

---
+++ UI_coin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_coin : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	public Text coinText;				//Textコンポーネント取得用
+	
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//coin数表示
+		coinText.text = gc.num_coin.ToString("00000");
+	}
+
+}
