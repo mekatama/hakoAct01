bb84109e04d2ac246717ce06f0147e2269dcc225
mekatama <tamatte@hotmail.com>
2018-04-12 19:24:15 +0900

    クリアui処理

--- GameController.cs
+++ GameController.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour {
 	public int num_coin = 0;	//coinの数
+	public int clear_coin = 2;	//clearに必要なコイン数
+	public bool isClear;		//clear flag
+
+	public Canvas clearCamvas;	//clear
 
 	void Start () {
+		isClear = false;				//初期化
+		clearCamvas.enabled = false;	//StartUI非表示
 	}
 	
 	void Update () {
+		//クリア判定
+		if(clear_coin == num_coin){
+			clearCamvas.enabled = true;	//StartUI表示
+			isClear = true;				//clear flag on
+		}
 		Debug.Log("coin;" + num_coin);
 	}
 }
