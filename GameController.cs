dd2f86afda529876c13991e747c7c471c5931b60
mekatama <tamatte@hotmail.com>
2018-04-13 21:55:22 +0900

    playtimeのカウント処理

--- GameController.cs
+++ GameController.cs
@@ -6,6 +6,7 @@ public class GameController : MonoBehaviour {
 	public int num_coin = 0;	//coinの数
 	public int clear_coin = 2;	//clearに必要なコイン数
 	public bool isClear;		//clear flag
+	public float timeCount;		//play時間
 
 	public Canvas clearCamvas;	//clear
 
@@ -15,11 +16,15 @@ public class GameController : MonoBehaviour {
 	}
 	
 	void Update () {
+		//timeカウント
+		timeCount += Time.deltaTime;	//play時間の保存
+		Debug.Log(timeCount);
+
 		//クリア判定
 		if(clear_coin == num_coin){
 			clearCamvas.enabled = true;	//StartUI表示
 			isClear = true;				//clear flag on
 		}
-		Debug.Log("coin;" + num_coin);
+//		Debug.Log("coin;" + num_coin);
 	}
 }
