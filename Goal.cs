435cfd6f1262b7dc2f335b3eb6bc5d20106f260e
mekatama <tamatte@hotmail.com>
2018-04-20 19:05:25 +0900

    goalのスクリプト

---
+++ Goal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	private bool in_Goal;				//
+	
+	//他のオブジェクトとの当たり判定
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Player"){
+			//gcって仮の変数にGameControllerのコンポーネントを入れる
+			GameController gc = gameController.GetComponent<GameController>();
+			if(gc.isClearCoin == true){
+				//
+				gc.inGoal = true;
+			}
+		}
+	}
+}
