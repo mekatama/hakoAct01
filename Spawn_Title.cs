63edcfcff07cddf5e7b34e654d2d6834f73b7a96
mekatama <tamatte@hotmail.com>
2018-04-20 19:06:01 +0900

    タイトル画面でplayer生成

---
+++ Spawn_Title.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Title : MonoBehaviour {
+	public GameObject player_Title = null;	//playerプレハブ
+	public float timeOut = 1.5f;			//playerの連射間隔
+	private float timeElapsed = 0.0f;		//playerの連射間隔カウント用
+	
+	void Update () {
+		timeElapsed += Time.deltaTime;
+        if(timeElapsed >= timeOut) {
+			Instantiate( player_Title, transform.position, transform.rotation);
+			timeElapsed = 0.0f;
+		}
+	}
+}
