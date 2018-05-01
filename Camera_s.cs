1034d16af108d297e4b4433c42e20bdf091cce35
mekatama <tamatte@hotmail.com>
2018-05-01 19:22:13 +0900

    demo cameraを動かす

---
+++ Camera_s.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_s : MonoBehaviour {
+	public float moveSpeed;	//cameraの移動speed
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector3(moveSpeed, 0, 0));
+	}
+}
