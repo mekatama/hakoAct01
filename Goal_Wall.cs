5ad0c5bada98da6610b1ebdf259d0fd49686abbb
mekatama <tamatte@hotmail.com>
2018-05-02 19:29:36 +0900

    goalの壁をcoin集めたら消す

---
+++ Goal_Wall.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal_Wall : MonoBehaviour {
+	GameObject gameController;	//gameController入れる用
+
+	void Start () {
+		gameController = GameObject.FindWithTag ("GameController");		//GameControllerオブジェクトを探す
+	}
+	
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//Goal_Wall消すかどうか判定
+		if(gc.isClearCoin == true){
+			Destroy(gameObject);
+		}
+	}
+}
