b6d2444b1a0a52ce1468f935096d9050e445bf33
mekatama <tamatte@hotmail.com>
2018-05-10 19:17:23 +0900

    ゴールmotion再生、coin取得se、goalSE

--- Player.cs
+++ Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
 	CharacterController characterController;//コンポーネントを入れる用
 	Animator animator;
 	public float graviy;
@@ -13,15 +14,25 @@ public class Player : MonoBehaviour {
 	private float moveSpeed = 1.5f;		//移動スピード
 	public bool isWalk;					//移動flag
 
+	AudioSource audioSource;			//AudioSourceコンポーネント取得用
+	public AudioClip audioClipCoin;		//coin SE
+	public AudioClip audioClipClear;	//clear SE
+	private bool onceClearCoin;			//一回だけ処理用
+
 	void Start () {
 		characterController = GetComponent<CharacterController>();	//コンポーネントを取得
 		animator = GetComponent<Animator>();	//コンポーネントを取得
 		targetPosition = transform.position;	//playerの位置
 		velocity = Vector3.zero;				//0ベクトル
 		isWalk = false;							//移動flag初期化
+		audioSource = gameObject.GetComponent<AudioSource>();		//AudioSourceコンポーネント取得
+		onceClearCoin = false;					//初期化
 	}
 
 	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+
 		//地上にいる時の処理
 		if(characterController.isGrounded){
 			velocity = Vector3.zero;
@@ -49,9 +60,33 @@ public class Player : MonoBehaviour {
 			velocity.y -= graviy * Time.deltaTime;	//重力の加算
 		}
 
-		//移動実行
-		characterController.Move(velocity * Time.deltaTime);
-		//animation制御
-		animator.SetBool("isWalk", isWalk);	
+		//入力許可onで移動
+		if(gc.isInPut){
+			//移動実行
+			characterController.Move(velocity * Time.deltaTime);
+		}else{
+			isWalk = false;	//移動flag off
+		}
+		animator.SetBool("isWalk", isWalk);		//animation制御
+
+		//goal侵入flag onで
+		if(gc.inGoal){
+			animator.SetBool("isFun", true);		//animation制御
+			//clear se 再生判定
+			if(onceClearCoin == false){
+				audioSource.clip = audioClipClear;	//SE決定
+				audioSource.Play ();				//SE再生
+				onceClearCoin = true;
+			}
+		}
+
+
+	}
+	//他のオブジェクトとの当たり判定
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "coin"){
+			audioSource.clip = audioClipCoin;	//SE決定
+			audioSource.Play ();				//SE再生
+		}
 	}
 }
