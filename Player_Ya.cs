45c267107324d037d08382a9004e52ae1fbe7464
mekatama <tamatte@hotmail.com>
2018-05-28 19:33:08 +0900

    goal矢印の向きとスケール制御

---
+++ Player_Ya.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Ya : MonoBehaviour {
+	public GameObject gameController;	//GameController取得
+	public 	Transform _target; 			//矢印の向くオブジェクト
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.LookAt (_target.position);
+
+		//表示on/off
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		if(gc.isClearCoin){
+			this.transform.localScale = new Vector3(1, 1, 1);
+		}else{
+			this.transform.localScale = new Vector3(0, 0, 0);
+		}
+
+
+	}
+}
