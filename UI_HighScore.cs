2bdcdeb438a417816225862fa2a2d0cc4ddfec1d
mekatama <tamatte@hotmail.com>
2018-05-31 19:49:43 +0900

    ハイスコアUIスクリプト

---
+++ UI_HighScore.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_HighScore : MonoBehaviour {
+
+	public GameObject gameController;	//GameController取得
+	public Text highScoreText;			//Textコンポーネント取得用
+
+	void Update () {
+		//gcって仮の変数にGameControllerのコンポーネントを入れる
+		GameController gc = gameController.GetComponent<GameController>();
+		//time表示
+		highScoreText.text = gc.highScoreTime.ToString("000.000");
+	}
+}
